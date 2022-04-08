1de976ce24bc72bd8b20b0cb078cfb8439c43ce9
jisrat19 <jisrat19@gmail.com>
2022-04-07 22:52:55 -0400

    eight

---
+++ src/sample5/Test11.java
@@ -0,0 +1,11 @@
+package sample5;
+
+public class Test11 {
+
+	public static void main(String [] args) {
+		System.err.println("Test 11");
+	}	
+	
+	
+	
+}
---
+++ src/sample5/Test12.java
@@ -0,0 +1,10 @@
+package sample5;
+
+public class Test12 {
+	public static void main(String [] args) {
+		System.err.println("Test 12");
+	}
+	
+	
+	
+}
